fdbadc94eed1f0568a7e7c430f523697c4be0761
Le Quang Hieu <105836140+hieuday1572@users.noreply.github.com>
2023-06-12 21:12:52 +0700

    demo

---
+++ LMMProject/LMMProject/Controllers/ADMINController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMMProject.Controllers
+{
+    public class ADMINController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
--- LMMProject/LMMProject/Controllers/LoginController.cs
+++ LMMProject/LMMProject/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using LMMProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MySqlX.XDevAPI;
 
 namespace LMMProject.Controllers
 {
@@ -28,9 +29,9 @@ namespace LMMProject.Controllers
             Account user = await _context.Accounts.Include(p => p.Role).FirstOrDefaultAsync(pro => pro.UserName.Equals(account.UserName));
             if (user != null)
             {
-                if (user.Password.Equals(account.Password))
+                if (user.Password.Equals(account.Password))      
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("DashBoard", "ADMIN");
                 }
                 TempData["Error"] = "Wrong: please try again!";
                 return View(account);

5f0174e387b44fccea1601afd84bcd42459d66cf
TTNGamers <126141777+TTNGamers@users.noreply.github.com>
2023-07-10 15:25:04 +0700

    new

---
+++ LMMProject/LMMProject/Controllers/UserSubjectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LMMProject.Data;
+using LMMProject.Models;
+
+namespace LMMProject.Controllers
+{
+    public class USERSubjectController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public USERSubjectController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subjects
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Subject.Include(s => s.Status);
+            return View(await appDbContext.ToListAsync());
+        }
+
+      

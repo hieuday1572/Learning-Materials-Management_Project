9b643f0000843fb077c60f3ca88677fdd6306152
TTNGamers <126141777+TTNGamers@users.noreply.github.com>
2023-07-14 21:41:30 +0700

    UserSyllabus

---
+++ LMMProject/LMMProject/Controllers/UserSyllabusController.cs
@@ -0,0 +1,77 @@
+﻿using LMMProject.Data;
+using LMMProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace LMMProject.Controllers
+{
+    public class UserSyllabusController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserSyllabusController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Syllabus
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string option, string search)
+        {
+            HttpContext.Session.SetString("option", option);
+            HttpContext.Session.SetString("search", search);
+
+            List<Syllabus> syllabus;
+
+            if (option.Equals("code"))
+            {
+                syllabus = await _context.Syllabus
+                    .Include(p => p.Assessments)
+                    .Where(p => p.SubjectCode.Contains(search.Trim()))
+                    .ToListAsync();
+            }
+            else
+            {
+                syllabus = await _context.Syllabus
+                    .Include(p => p.Assessments)
+                    .Where(p => p.SyllabusNameEn.Contains(search) || p.SyllabusNameVn.Equals(search.Trim()))
+                    .ToListAsync();
+            }
+
+            if (syllabus.Count() == 0)
+            {
+                TempData["error"] = "Sorry: Syllabus not found!";
+            }
+
+            return View(syllabus);
+        }
+
+
+
+
+        // GET: Syllabus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var syllabus = await _context.Syllabus
+                .Include(s => s.Decision)
+                .Include(s => s.Assessments)
+                .FirstOrDefaultAsync(m => m.SyllabusId == id);
+            if (syllabus == null)
+            {
+                return NotFound();
+            }
+
+            return View(syllabus);
+        }
+
+    }
+}

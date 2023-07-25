bd090275a9f04f3cee49fbaa0c9a9470dfdda29e
Le Quang Hieu <105836140+hieuday1572@users.noreply.github.com>
2023-07-25 22:59:57 +0700

    update

---
+++ LMMProject/LMMProject/Controllers/ADMINAccountsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LMMProject.Data;
+using LMMProject.Models;
+
+namespace LMMProject.Controllers
+{
+    public class ADMINAccountsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ADMINAccountsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ADMINAccounts
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Account.Include(a => a.Role);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: ADMINAccounts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Account == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account
+                .Include(a => a.Role)
+                .FirstOrDefaultAsync(m => m.UserName == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: ADMINAccounts/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Role, "RoleId", "RoleId");
+            return View();
+        }
+
+        // POST: ADMINAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserName,Password,RoleId")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                account.Active = 1;
+                Account check = _context.Account.SingleOrDefault(p=>p.UserName.Trim().Equals(account.UserName.Trim()));
+                if (check != null)
+                {
+                    TempData["Error"] = "Wrong: Username is already exist !";
+                    return View(account);
+                }
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: ADMINAccounts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Account == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        // POST: ADMINAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.UserName))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: ADMINAccounts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Account == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Account
+                .Include(a => a.Role)
+                .FirstOrDefaultAsync(m => m.UserName == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: ADMINAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Account == null)
+            {
+                return Problem("Entity set 'AppDbContext.Account'  is null.");
+            }
+            var account = await _context.Account.FindAsync(id);
+            if (account != null)
+            {
+                _context.Account.Remove(account);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(string id)
+        {
+          return (_context.Account?.Any(e => e.UserName == id)).GetValueOrDefault();
+        }
+    }
+}
